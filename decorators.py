006e97aec2613e9f4d94e9f5324599408e253cf8
Denis Bilokon <greenjuiced@gmail.com>
2023-01-26 18:16:29 +0200

    added exception for Mail

--- decorators.py
+++ decorators.py
@@ -1,4 +1,4 @@
-from bot_classes import WrongTypePhone, WrongLenPhone
+from bot_classes import WrongTypePhone, WrongLenPhone, WrongMail
 
 
 def input_error(func):
@@ -10,12 +10,14 @@ def input_error(func):
         except KeyError:
             return "Enter correct username"
         except ValueError:
-            return "Enter username"
+            return "Incorrect input. Try again"
         except TypeError:
             return "Not enough params for command"
         except WrongLenPhone:
             return "Length of phone's number is wrong"
         except WrongTypePhone:
             return 'Incorrect phone number'
+        except WrongMail:
+            return 'User already have email. You can only change or delete'
 
     return wrapper
