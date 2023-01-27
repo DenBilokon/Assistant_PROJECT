c53f345027ec1868d310b7749bc874c4c60ef3c7
Denis Bilokon <greenjuiced@gmail.com>
2023-01-27 22:04:43 +0200

    Зробив костяк для додатків. added defs: 1. add_note 2. remove_note 3. show_all

--- notes.py
+++ notes.py
@@ -1,6 +1,29 @@
-
+from notes_classes import Note, Tag, NOTEBOOK, Record
 
 HELP_TEXT = """This contact bot save your contacts 
     Global commands:
       'add contact' - add new contact. Input user name and phone
     Example: add User_name 095-xxx-xx-xx
+    """
+
+
+def add_note(message, tag):
+    record = Record(message, tag)
+    NOTEBOOK.add_note(record)
+
+
+def delete_note(note_index):
+    NOTEBOOK.remove_note(note_index)
+
+
+def show_all():
+    print(NOTEBOOK.show_all())
+
+
+note = input('print note ')
+tag = input('print tag ')
+add_note(note, tag)
+show_all()
+index = input('print index ')
+delete_note(int(index))
+show_all()
--- notes_classes.py
+++ notes_classes.py
@@ -1,8 +1,11 @@
 from collections import UserDict
 import pickle
 
-
 class NoteBook(UserDict):
+    def __init__(self):
+        super().__init__()
+        self.index = 1
+
     def read_file(self):
         with open('NoteBook.bin', 'rb') as reader:
             self.data = pickle.load(reader)
@@ -13,42 +16,50 @@ class NoteBook(UserDict):
             pickle.dump(self.data, writer)
 
     def add_note(self, record):
-        self.data[record.index.value] = record
+        if self.index:
+            self.data[self.index] = record
+            self.index += 1
+        return self.index
 
-    def remove_note(self, record):
-        self.data.pop(record.index.value, None)
+    def remove_note(self, index):
+        self.data.pop(index, None)
+
+    def show_all(self):
+        return "\n".join(f'{index} {", ".join([p.value for p in wr.tags])} {wr.note.value}' for index, wr in self.data.items())
 
 
 class Field:
     def __init__(self, value):
-        self._value = value
-
-    def __str__(self):
-        return self._value
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, value):
-        self._value = value
+        self.value = value
 
 
-class Tags(Field):
+class Tag(Field):
     pass
 
 
-class Notes(Field):
-    def __str__(self):
-        return str(self.value)
+class Note(Field):
+    pass
 
-    def __repr__(self):
-        return str(self.value)
 
-    @Field.value.setter
-    def value(self, value):
-        self._value = value
+class Record:
+    def __init__(self, note, tag=None):
+        self.note = Note(note)
+        self.tags = []
+        if tag:
+            self.tags.append(Tag(tag))
 
 
 NOTEBOOK = NoteBook()
+# s = Note('asfafsafs')
+# a = Note('235235')
+# g = Note('23525dsf')
+# rec = Record(s)
+# rec1 = Record(a)
+# ab = NoteBook()
+# ab.add_note(rec)
+# ab.add_note(rec1)
+
+
+# print(ab.show_all())
+# er = ab.get(key)
+# print(er)
