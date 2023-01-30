9e44495ec8228b6bccb5828c656f39bd5cdf1cef
Denis Bilokon <greenjuiced@gmail.com>
2023-01-30 11:27:44 +0200

    change setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name='Help_Assistant',
-    version='0.0.2',
+setup(name='help-assistant',
+    version='0.0.6',
     description= 'Addressbook, notebook, clean folder',
     url='https://github.com/DenBilokon/Assistant_PROJECT',
     author='Denis Bilokon, Kirill Sheremeta, Denys Zaycev, Dmytro Marchenko, Maria Palona',
